3bff039c25bb0675aa85b4618ed12e82d374e638
PhanAnh1811 <tuananh.61779@gmail.com>
2021-05-27 18:14:26 +0700

    update

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import MainNavigation from './src/navigation/MainNavigation';
 import HomeScreen from './src/screens/HomeScreen'
-import AddHomeScreen from './src/screens/AddHomeScreen'
+import AddHomeScreen from './src/screens/AddRoomScreen'
 import RoomScreen from './src/screens/RoomScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import SignupScreen from './src/screens/SignupScreen'
 
 const App=()=>{
   return(
-      <LoginScreen />
+      <MainNavigation />
   )
 }
 
---
+++ src/components/CustomButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import Colors from '../utils/Colors'
+
+import {Button} from 'react-native-paper';
+
+export default function CustomButton({ name, navigation, screen,modeValue,...res }) {
+    return (
+        <View style={styles.btn}>
+            <TouchableOpacity onPress={() => navigation.navigate(screen)}>
+                <Text style={{
+                    textAlign: 'center',
+                    fontSize: 25,
+                    color: 'white',
+                    marginTop: 5,
+                    fontWeight:'bold'
+                }}> {name} </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    btn: {
+        backgroundColor: Colors.purple,
+        width: 120,
+        height: 50,
+        marginLeft: 120,
+        borderRadius: 20,
+    },
+})
